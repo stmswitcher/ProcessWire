1f00f54a40b3921b68bb8ba1927a4bf51e0a0a1d
Denis Alexandrov <stm.switcher@gmail.com>
2015-12-22 14:34:19 +0300

    [M] Merge modifications to the base version

--- index.php
+++ index.php
@@ -4,21 +4,26 @@
  * ProcessWire Bootstrap
  *
  * This file may be used to bootstrap either the http web accessible
- * version, or the command line client version of ProcessWire. 
+ * version, or the command line client version of ProcessWire.
  *
  * Note: if you happen to change any directory references in here, please
  * do so after you have installed the site, as the installer is not informed
- * of any changes made in this file. 
- * 
- * ProcessWire 2.x 
- * Copyright (C) 2015 by Ryan Cramer 
+ * of any changes made in this file.
+ *
+ * ProcessWire 2.x
+ * Copyright (C) 2015 by Ryan Cramer
  * Licensed under Mozilla Public License v2.0 http://mozilla.org/MPL/2.0/
- * 
+ *
  * https://processwire.com
  *
  * @version 2.7
  *
  */
+
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?: 'production'));
+
+if (APPLICATION_ENV == 'production' && is_file("./auth.php"))
+    require('./auth.php');
 
 define("PROCESSWIRE", 270); // index version
 
@@ -35,7 +40,7 @@ function ProcessWireBootConfig() {
 	 *
 	 */
 	$rootPath = dirname(__FILE__);
-	if(DIRECTORY_SEPARATOR != '/') $rootPath = str_replace(DIRECTORY_SEPARATOR, '/', $rootPath); 
+	if(DIRECTORY_SEPARATOR != '/') $rootPath = str_replace(DIRECTORY_SEPARATOR, '/', $rootPath);
 
 	if(isset($_SERVER['HTTP_HOST'])) {
 		$httpHost = strtolower($_SERVER['HTTP_HOST']);
@@ -45,7 +50,7 @@ function ProcessWireBootConfig() {
 
 		// check if we're being included from another script and adjust the rootPath accordingly
 		$sf = empty($_SERVER['SCRIPT_FILENAME']) ? '' : dirname(realpath($_SERVER['SCRIPT_FILENAME']));
-		$f = dirname(realpath(__FILE__)); 
+		$f = dirname(realpath(__FILE__));
 		if($sf && $sf != $f && strpos($sf, $f) === 0) {
 			$x = rtrim(substr($sf, strlen($f)), '/');
 			$rootURL = substr($rootURL, 0, strlen($rootURL) - strlen($x));
@@ -61,9 +66,9 @@ function ProcessWireBootConfig() {
 	/*
 	 * Allow for an optional /index.config.php file that can point to a different site configuration per domain/host.
 	 *
-	 */     
-	$siteDir = 'site'; 
-	$indexConfigFile = $rootPath . "/index.config.php"; 
+	 */
+	$siteDir = 'site';
+	$indexConfigFile = $rootPath . "/index.config.php";
 
 	if(is_file($indexConfigFile)) {
 		// optional config file is present in root
@@ -71,7 +76,7 @@ function ProcessWireBootConfig() {
 		$hostConfig = function_exists("ProcessWireHostSiteConfig") ? ProcessWireHostSiteConfig() : array();
 		if($httpHost && isset($hostConfig[$httpHost])) $siteDir = $hostConfig[$httpHost];
 			else if(isset($hostConfig['*'])) $siteDir = $hostConfig['*']; // default override
-	} 
+	}
 
 	// other default directories
 	$wireDir = 'wire';
@@ -90,12 +95,12 @@ function ProcessWireBootConfig() {
 	 *
 	 */
 	$config = new Config();
-	$config->urls = new Paths($rootURL); 
+	$config->urls = new Paths($rootURL);
 	$config->urls->wire = "$wireDir/";
 	$config->urls->site = "$siteDir/";
 	$config->urls->modules = "$wireDir/modules/";
 	$config->urls->siteModules = "$siteDir/modules/";
-	$config->urls->core = "$coreDir/"; 
+	$config->urls->core = "$coreDir/";
 	$config->urls->assets = "$assetsDir/";
 	$config->urls->cache = "$assetsDir/cache/";
 	$config->urls->logs = "$assetsDir/logs/";
@@ -103,7 +108,7 @@ function ProcessWireBootConfig() {
 	$config->urls->tmp = "$assetsDir/tmp/";
 	$config->urls->templates = "$siteDir/templates/";
 	$config->urls->adminTemplates = is_dir("$siteDir/$adminTplDir") ? "$siteDir/$adminTplDir/" : "$wireDir/$adminTplDir/";
-	$config->paths = clone $config->urls; 
+	$config->paths = clone $config->urls;
 	$config->paths->root = $rootPath . '/';
 	$config->paths->sessions = $config->paths->assets . "sessions/";
 
@@ -121,19 +126,18 @@ function ProcessWireBootConfig() {
 	 */
 	include("$rootPath/$wireDir/config.php");
 	$configFile = "$rootPath/$siteDir/config.php";
-	$configFileDev = "$rootPath/$siteDir/config-dev.php";
-	@include(is_file($configFileDev) ? $configFileDev : $configFile); 
+	include($configFile);
 
 	/*
 	 * $config->debugIf: optional setting to determine if debug mode should be on or off
-	 * 
+	 *
 	 */
 	if($config->debugIf && is_string($config->debugIf)) {
 		$debugIf = trim($config->debugIf);
 		if(strpos($debugIf, '/') === 0) $debugIf = (bool) @preg_match($debugIf, $_SERVER['REMOTE_ADDR']); // regex IPs
-			else if(is_callable($debugIf)) $debugIf = $debugIf(); // callable function to determine debug mode for us 
+			else if(is_callable($debugIf)) $debugIf = $debugIf(); // callable function to determine debug mode for us
 			else $debugIf = $debugIf === $_SERVER['REMOTE_ADDR']; // exact IP match
-		$config->debug = $debugIf; 
+		$config->debug = $debugIf;
 	}
 
 	/*
@@ -152,14 +156,14 @@ function ProcessWireBootConfig() {
 	 * If PW2 is not installed, go to the installer
 	 *
 	 */
-	if(!$config->dbName) { 
+	if(!$config->dbName) {
 		if(is_file("./install.php") && strtolower($_SERVER['REQUEST_URI']) == strtolower($rootURL)) {
 			require("./install.php");
 			exit(0);
 		} else {
 			header("HTTP/1.1 404 Page Not Found");
 			echo "404 page not found (no site configuration or install.php available)";
-			exit(0); 
+			exit(0);
 		}
 	}
 
@@ -167,23 +171,23 @@ function ProcessWireBootConfig() {
 	 * Prepare any PHP ini_set options
 	 *
 	 */
-	session_name($config->sessionName); 
-	ini_set('session.use_cookies', true); 
+	session_name($config->sessionName);
+	ini_set('session.use_cookies', true);
 	ini_set('session.use_only_cookies', 1);
-	ini_set('session.cookie_httponly', 1); 
+	ini_set('session.cookie_httponly', 1);
 	ini_set('session.gc_maxlifetime', $config->sessionExpireSeconds);
 
 	if(ini_get('session.save_handler') == 'files') {
 		if(ini_get('session.gc_probability') == 0) {
 			// Some debian distros replace PHP's gc without fully implementing it,
-			// which results in broken garbage collection if the save_path is set. 
-			// As a result, we avoid setting the save_path when this is detected. 
+			// which results in broken garbage collection if the save_path is set.
+			// As a result, we avoid setting the save_path when this is detected.
 		} else {
 			ini_set("session.save_path", rtrim($config->paths->sessions, '/'));
 		}
 	}
 
-	return $config; 
+	return $config;
 }
 
 /**
@@ -192,22 +196,22 @@ function ProcessWireBootConfig() {
  */
 function ProcessWireExternalShutdown() {
 	if(error_get_last()) return;
-	$process = wire('process'); 
+	$process = wire('process');
 	if($process == 'ProcessPageView') $process->finished();
 }
 
 /**
  * Determine if the request is one we should handle (internal) or one that another
- * script coming after this (external) will handle. 
+ * script coming after this (external) will handle.
  *
  */
 $internal = isset($_SERVER['HTTP_HOST']) && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME']);
-if(!$internal) register_shutdown_function('ProcessWireExternalShutdown'); 
+if(!$internal) register_shutdown_function('ProcessWireExternalShutdown');
 
 /*
  * If you include ProcessWire's index.php from another script, or from a
- * command-line script, the $wire variable or wire() function is your 
- * connection to the API of this ProcessWire instance. 
+ * command-line script, the $wire variable or wire() function is your
+ * connection to the API of this ProcessWire instance.
  *
  */
 $process = null;
@@ -217,21 +221,21 @@ $wire = null;
  * Build the ProcessWire configuration
  *
  */
-$config = ProcessWireBootConfig(); 
+$config = ProcessWireBootConfig();
 
 /*
  * Load and execute ProcessWire
  *
  */
-try { 
-	
+try {
+
 	/*
 	 * Bootstrap ProcessWire's core and make the API available with $wire or wire()
 	 *
 	 */
 	$wire = new ProcessWire($config);
 	$process = $wire->modules->get('ProcessPageView');
-	$wire->wire('process', $process); 
+	$wire->wire('process', $process);
 	echo $process->execute($internal);
 	$internal ? $process->finished() : extract($wire->wire('all')->getArray());
 
@@ -245,6 +249,6 @@ try {
 	$wire ? $wire->trackException($e) : $config->trackException($e);
 	$errorMessage = "Exception: " . $e->getMessage() . " (in " . $e->getFile() . " line " . $e->getLine() . ")";
 	if($config->debug || ($wire && $wire->user && $wire->user->isSuperuser())) $errorMessage .= "\n\n" . $e->getTraceAsString();
-	trigger_error($errorMessage, E_USER_ERROR); 
+	trigger_error($errorMessage, E_USER_ERROR);
 }
 
